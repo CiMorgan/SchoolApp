aecd8c50b76eebf4d5aa552b7ab9695408f75404
CiMorgan <camorgan70@gmail.com>
2021-02-16 18:45:23 -0500

    Feb 16 push 1

--- SchoolWebApi/Models/Student.cs
+++ SchoolWebApi/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,24 @@ namespace SchoolWebApi.Models
 {
     public class Student
     {
+        public enum LevelOfGrade
+        {
+            Ninth,         
+            Tenth,
+            Eleventh,
+            Twelfth
+        }
+
+        [PrimaryKey]
         public int Id { get; set; }
         public string Name { get; set; }
-        public int GradeLevel { get; set; }
+        public LevelOfGrade GradeLevel { get; set; }
         public bool Expelled { get; set; }
+        [ForeignKey("Activity")]
+        public virtual int Activities { get; set; }
+        [ForeignKey("Course")]
+        public virtual int Courses { get; set; }
+        [ForeignKey("Discipline")]
+        public virtual int DisciplineRecord { get; set; }
     }
 }
