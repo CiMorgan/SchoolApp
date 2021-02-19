dd1ec1029ac366edf8669cceae2ac426835ef04f
Ben Thomas <bmthomas7@gmail.com>
2021-02-18 21:08:23 -0500

    Teacher

--- School.Data/Teacher.cs
+++ School.Data/Teacher.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace SchoolWebApi.Models
+namespace School.Data
 {
     public class Teacher
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-        //public virtual DepartmentName Department { get; set; }
+        public enum DepartmentName
+        {
+            English = 1,
+            Math,
+            Science,
+            SocialStudies,
+            WorldLanguages,
+            Art,
+            PhysicalEducation
+        }
+
+        public DepartmentName Department { get; set; }
+
+
+        public virtual ICollection<Course> CourseList { get; set; }
+
+        public Teacher()
+        {
+            CourseList = new HashSet<Course>();
+
+        }
     }
 }
