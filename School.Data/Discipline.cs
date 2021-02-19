077999ad936ce9cd841e7f1e2bb980c3a3a11e8b
Ben Thomas <bmthomas7@gmail.com>
2021-02-18 21:05:43 -0500

    Discipline

--- School.Data/Discipline.cs
+++ School.Data/Discipline.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace SchoolWebApi.Models
+namespace School.Data
 {
     public class Discipline
     {
+        [Key]
         public int Id { get; set; }
         public DateTime Date { get; set; }
+
+        //attribute on comment
         public string Comment { get; set; }
-        public bool Expelled  { get; set; }
-        public enum Type
+        public bool Expelled { get; set; }
+        public enum TypeOfDiscipline
         {
             Detention = 1,
             InSchoolSuspension,
             OutOfSchoolSuspension,
-            Expulsion,
+            Expulsion
         }
-        public Type DisciplineType { get; set; }
+        public TypeOfDiscipline DisciplineType { get; set; }
+
+        public virtual ICollection<Student> StudentList { get; set; }
+
+        public Discipline()
+        {
+            StudentList = new HashSet<Student>();
+        }
     }
 }
